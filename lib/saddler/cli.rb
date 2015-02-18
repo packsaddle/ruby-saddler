5cc8ab886f65644828fe9e523f06e49aab8367b0
sanemat <o.gata.ken@gmail.com>
2015-02-18 17:05:36 +0900

    feat(cli): show version

---
+++ lib/saddler/cli.rb
@@ -0,0 +1,16 @@
+require 'thor'
+
+module Saddler
+  class CLI < Thor
+    def self.exit_on_failure?
+      true
+    end
+
+    desc 'version', 'Show the Saddler version'
+    map %w(-v --version) => :version
+
+    def version
+      puts "Saddler version #{::Saddler::VERSION}"
+    end
+  end
+end
