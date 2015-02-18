ffbaad7287ad0f2be7e85c300bab3a91c115c0fb
sanemat <o.gata.ken@gmail.com>
2015-02-18 18:35:43 +0900

    test(test): remove unnecessary test

--- test/test_saddler.rb
+++ test/test_saddler.rb
@@ -4,8 +4,4 @@ class TestSaddler < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Saddler::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
