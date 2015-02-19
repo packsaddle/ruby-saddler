e9933c21738ad88893657f099102032a7dd7aca3
sanemat <o.gata.ken@gmail.com>
2015-02-20 01:33:46 +0900

    refactor(cli): remove unnecessary variable

--- lib/saddler/cli.rb
+++ lib/saddler/cli.rb
@@ -32,15 +32,13 @@ module Saddler
 
       abort('no input') if !data || data.empty?
 
-      pass_options = options[:options]
-
       require options[:require] if options[:require]
       if(options[:reporter])
         reporter = ::Saddler::Reporter.add_reporter(options[:reporter], $stdout)
       end
 
       abort('no reporter') unless reporter
-      reporter.report(data, pass_options)
+      reporter.report(data, options[:options])
     end
   end
 end
