e56ec137327fb0fe211d2379bad670313a45a5db
sanemat <o.gata.ken@gmail.com>
2015-03-01 18:11:14 +0900

    feat(logger): add logger

--- lib/saddler.rb
+++ lib/saddler.rb
@@ -1,6 +1,21 @@
+require 'logger'
 require 'saddler/version'
 require 'saddler/reporter'
 
 module Saddler
-  # Your code goes here...
+  def self.default_logger
+    logger = Logger.new(STDERR)
+    logger.progname = 'Saddler'
+    logger.level = Logger::WARN
+    logger
+  end
+
+  def self.logger
+    return @logger if @logger
+    @logger = default_logger
+  end
+
+  class << self
+    attr_writer :logger
+  end
 end
