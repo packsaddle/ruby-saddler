ba91b1550118f8b3e6b3deda782b6d9312748785
sanemat <o.gata.ken@gmail.com>
2015-03-02 18:25:54 +0900

    chore(saddler): setting

---
+++ bin/run-rubocop.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -ev
+if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+  # Travis-CI
+  #
+  # git clone --depth=50 \
+  # git://github.com/packsaddle/example-ruby-travis-ci.git \
+  # packsaddle/example-ruby-travis-ci
+  # cd packsaddle/example-ruby-travis-ci
+  # git fetch origin +refs/pull/1/merge:
+  # git checkout -qf FETCH_HEAD
+
+  echo gem install
+  gem install --no-document rubocop-select rubocop rubocop-checkstyle_formatter \
+              checkstyle_filter-git saddler saddler-reporter-github
+
+  echo git diff
+  git diff -z --name-only origin/master
+
+  echo rubocop-select
+  git diff -z --name-only origin/master \
+   | xargs -0 rubocop-select
+
+  TARGET_FILES=$(git diff -z --name-only origin/master \
+                 | xargs -0 rubocop-select)
+
+  if [ "${TARGET_FILES}" == "" ]; then
+    echo "not found"
+    exit 0
+  fi
+
+  echo rubocop
+  # rubocop return 1 both detecting error and raise error
+  RUBOCOP_RESULT=$(git diff -z --name-only origin/master \
+   | xargs -0 rubocop-select \
+   | xargs rubocop \
+       --require rubocop/formatter/checkstyle_formatter \
+       --format RuboCop::Formatter::CheckstyleFormatter)
+  echo $RUBOCOP_RESULT
+
+  echo checkstyle_filter-git
+  git diff -z --name-only origin/master \
+   | xargs -0 rubocop-select \
+   | xargs rubocop \
+       --require rubocop/formatter/checkstyle_formatter \
+       --format RuboCop::Formatter::CheckstyleFormatter \
+   | checkstyle_filter-git diff origin/master
+
+  echo saddler
+  git diff -z --name-only origin/master \
+   | xargs -0 rubocop-select \
+   | xargs rubocop \
+       --require rubocop/formatter/checkstyle_formatter \
+       --format RuboCop::Formatter::CheckstyleFormatter \
+   | checkstyle_filter-git diff origin/master \
+   | saddler report \
+      --require saddler/reporter/github \
+      --reporter Saddler::Reporter::Github::PullRequestReviewComment
+fi
+
+exit 0
